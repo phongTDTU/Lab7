3f964372588c6e94511bb01c4ecf65c8ee2e3e32
thieuphong <tangthieuphong888@gmail.com>
2023-04-01 00:11:07 +0700

    user2 update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,6 @@
 class demo {
     public static void main(String[] args) {
-        System.out.println("hello frome user1");
+        System.out.println("hello from user1");
+        System.out.println("hello from user2");
     }
 }
